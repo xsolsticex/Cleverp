d5e618e23833960abab724cad7daba63b22fb7ca
Carlos Jimenez <jimenez.martin.carlos@alumnat.copernic.cat>
2023-03-27 21:31:43 +0200

    Revert "Corrección de errores y eliminacion de html's"
    This reverts commit 64dafac1bc56730c9599d6ceb53e14ae18245505.

--- src/main/java/com/example/cleverp/controladors/ConfiguracioWeb.java
+++ src/main/java/com/example/cleverp/controladors/ConfiguracioWeb.java
@@ -21,7 +21,7 @@ public class ConfiguracioWeb implements WebMvcConfigurer {
     */
     @Override
     public void addViewControllers(ViewControllerRegistry registre) {
-        registre.addViewController("/").setViewName("base"); //Mostrem la pàgina incial que reanomenen com a index, quan encara no ens hem autenticat
+        registre.addViewController("/").setViewName("index"); //Mostrem la pàgina incial que reanomenen com a index, quan encara no ens hem autenticat
         registre.addViewController("/login"); //Mostrem la pàgina login quan l'usuari no ha pogut autenticar-se
         registre.addViewController("/errors/error403").setViewName("/errors/error403"); //Mostrem la pàgina error403 quan l'usuari no pot accedir a una pàgina determinada.
     }
