6d6254565f056fe276f26a7cdd445b5dd5aa985d
Brian Camps Martínez <camps.martinez.brian@alumnat.copernic.cat>
2023-04-18 19:21:30 +0200

    Modificar error403.html

--- src/main/java/com/example/cleverp/controladors/ConfiguracioAutenticacio.java
+++ src/main/java/com/example/cleverp/controladors/ConfiguracioAutenticacio.java
@@ -11,10 +11,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-/**
- *
- * @author fta
- */
 
 /*Classe de configuració de Spring Security per configurar l'accés d'usuaris (autenticació).
  *Aquesta classe ha d'extendre de la classe WebSecurityConfigurerAdapter de Spring Security per poder
@@ -29,7 +25,7 @@ public class ConfiguracioAutenticacio {
     private UserDetailsService userDetailsService; //Objecte per recuperar l'usuari
 
     /*AUTENTICACIÓ*/
-    /*Injectem mitjançant @Autowired, els mètodes de la classe AuthenticationManagerBuilder. Mitjançant
+ /*Injectem mitjançant @Autowired, els mètodes de la classe AuthenticationManagerBuilder. Mitjançant
      *aquesta classe cridarem al mètode userDetailsService de la classe AuthenticationManagerBuilder què és el mètode que
      *realitzarà l'autenticació. Per parm̀etre el sistema li passa l'usuari introduit en el formulari d'autenticació.
      *Aquest usuari ens el retorna el mètode loadUserByUsername implementat en UsuariService.
@@ -45,40 +41,38 @@ public class ConfiguracioAutenticacio {
 
 
     /*AUTORITZACIÓ*/
-    /*Utilitzem el mètode filterChain per configurar l'accés dels nostre usuaris a l'aplicació, segons
+ /*Utilitzem el mètode filterChain per configurar l'accés dels nostre usuaris a l'aplicació, segons
      *els seus rols, el que s'anomena autoritzar.
      *Passem com a paràmetre un objecte de la classe HttpSecurity de Spring Security que
      *és el que ens permetrà cridar als mètodes per configurar les restriccions d'accés a la nostra aplicació.
-    */
-
-   
+     */
     @Bean //L'indica al sistema que el mètode és un Bean, en aquest cas perquè crea un objecte de la classe HttpSecurity
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        
+
         String[] resourcesStatic = {"/Cleverp.png/**", "/background.js/**", "/signin.css/**", "/styles.css/**"};//Arxius utilitzats per el login.
-        String[] resourcesStatic2 = {"../../Cleverp.png/**", "../../background.js/**", "../../signin.css/**", "../../styles.css/**"};//Arxius utilitzats per el login.
-        
+        String[] resourcesStatic2 = {"../../Cleverp.png/**", "/../../background.js/**", "/../../signin.css/**", "/../../styles.css/**"};//Arxius utilitzats per el login.
+
         return http.authorizeHttpRequests((requests) -> requests
-        	 //En el nostre cas el mètode hasAnyAuthority fa el mateix que HasAnyRoles, o hasAuthority el mateix que hasRol, però en aquesta nova versió per autoritzar els usuaris, els mètodes
-        	 //dels rols, normalment donen problemes, els Authority, no.
+                //En el nostre cas el mètode hasAnyAuthority fa el mateix que HasAnyRoles, o hasAuthority el mateix que hasRol, però en aquesta nova versió per autoritzar els usuaris, els mètodes
+                //dels rols, normalment donen problemes, els Authority, no.
                 .requestMatchers(resourcesStatic).permitAll()
                 .requestMatchers(resourcesStatic2).permitAll()
-                .requestMatchers("/base/**").hasAnyAuthority("administrador", "arbitre", "venedor") //URL iniciGossos on pot accedir el rol de veterinari o pacient
+                .requestMatchers("/base/**").hasAnyAuthority("administrador", "arbitre", "venedor")
                 .requestMatchers("/hola/**").hasAnyAuthority("administrador")
                 .requestMatchers("/holaVenedor/**").hasAnyAuthority("arbitre", "venedor")
                 .requestMatchers("/empleats/**").hasAnyAuthority("administrador")
                 .requestMatchers("/editar/empleat/**").hasAnyAuthority("administrador")
                 .requestMatchers("/elimina/empleat/**").hasAnyAuthority("administrador")
-                .requestMatchers("/empleat/nou/**").hasAnyAuthority("administrador")
+                .requestMatchers("/empleat/nou").hasAnyAuthority("administrador")
                 .anyRequest().authenticated() //Qualsevol altre sol.licitud que no coincideixi amb les regles anteriors cal autenticació
-                )
+        )
                 .formLogin((form) -> form //Objecte que representa el formulari de login personalitzat que utilitzarem
-                .loginPage("/login")  //Pàgina on es troba el formulari per fer login personalitzat
+                .loginPage("/login") //Pàgina on es troba el formulari per fer login personalitzat
                 .permitAll() //Permet acceddir a tothom
                 )
                 .exceptionHandling((exception) -> exception //Quan es produeix una excepcció 403, accés denegat, mostrem el nostre missatge
                 .accessDeniedPage("/error403"))
-                .logout((logout) -> logout  //Habilita el log out
+                .logout((logout) -> logout //Habilita el log out
                 .permitAll() //Permitir a todos
                 )
                 .build();
